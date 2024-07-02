e09ceb4b7aa2b7117dd4705651ccfc06d9eaa626
WaveGuide1 <barthalaekwe@gmail.com>
2024-07-02 11:30:10 +0100

    final commit

--- weather/views.py
+++ weather/views.py
@@ -9,13 +9,9 @@ logger = logging.getLogger(__name__)
 
 class GreetingView(View):
     def get(self, request):
-        visitor_name = request.GET.get('name')
-        client_ip = request.META.get('HTTP_X_FORWARDED_FOR', '') or request.META.get('REMOTE_ADDR')
-
-        if client_ip in ('127.0.0.1', 'localhost'):
-            location = 'Nigeria'
-        else:
-            location = self.get_location(client_ip)
+        visitor_name = request.GET.get('name', 'Guest')
+        client_ip = self.get_client_ip(request)
+        location = self.get_location(client_ip)
 
         if location == 'Unknown':
             return JsonResponse({
@@ -43,17 +39,30 @@ class GreetingView(View):
         return JsonResponse({
             'clientIp': client_ip,
             'location': location,
-            'greeting': f'Hello, {visitor_name}!, the temperature is {temperature} in {location}'
+            'greeting': f'Hello, {visitor_name}! The temperature is {temperature} in {location}'
         })
 
+    def get_client_ip(self, request):
+        client_ip = request.META.get('HTTP_X_FORWARDED_FOR')
+        if client_ip:
+            client_ip = client_ip.split(',')[0]
+        else:
+            client_ip = request.META.get('REMOTE_ADDR')
+        return client_ip
+
     def get_location(self, client_ip):
+        if client_ip in ('127.0.0.1', 'localhost'):
+            return 'Nigeria'
         url = f'https://freegeoip.app/json/{client_ip}'
         try:
             response = requests.get(url)
             response.raise_for_status()
             try:
                 response_data = response.json()
-                return response_data.get('city', 'Unknown')
+                city = response_data.get('city')
+                if city:
+                    return city
+                return response_data.get('country_name', 'Unknown')
             except ValueError as e:
                 logger.error(f"Error decoding JSON from {url}: {e}")
                 logger.error(f"Response content: {response.content}")
